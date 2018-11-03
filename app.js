0ace5b457e27cbfc50d846fc90514687c449fb1b
Abhyudaya Sharma <sharmaabhyudaya@gmail.com>
2018-11-03 10:47:21 +0530

    Send image from browser to node

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 const app = express();
 
-app.use('/public', express.static('public'));
+app.use('/public', express.static('public')); // expose public
+app.use(bodyParser.json({limit: '50mb', type: 'application/json'}));
 
 app.get('/', async (req, res) => {
-  // let gcode = await require('./src/gcodegenerator')
-  //     .generateGCode('./test/lines.png', {toolDiameter: 1});
-  // gcode = gcode.toString();
-  // res.send(gcode.replace(/\n/g, '<br />'));
   res.sendFile('index.html', {root: __dirname}, (err) => {
     if (err) {
       res.send(`ERROR: ${err}`);
+    }
+  });
+});
+
+app.post('/gcode', (request, response) => {
+  console.log('Got post request');
+  let fileType;
+  switch (request.body.type) {
+    case 'image/jpeg':
+      fileType = '.jpeg';
+      break;
+    case 'image/png':
+      fileType = '.png';
+      break;
+    default:
+      response.status(400).send('Unrecognized file type');
+      return;
+  }
+
+  const filePath = require('path').resolve(`./temp/${Date.now()}${fileType}`);
+  fs.writeFile(filePath, request.body.data, 'binary', (err) => {
+    if (err) {
+      response.status(400).send(`error: ${err}`);
+    } else {
+      response.send(`File written at: ${filePath}`);
     }
   });
 });
--- public/js/browse.js
+++ public/js/browse.js
@@ -1,10 +1,63 @@
+/* eslint-disable no-unused-vars */
 /**
  * Button click
  */
 function buttonClicked() {
   const file = document.getElementById('image_file').value;
   if (file != null) {
-    document.getElementById('image').innerHTML = `<img src=${file}>`;
+    document.getElementById('image').innerHTML = `<img src="${file}>"`;
+  }
+  if (window.File && window.FileReader && window.FileList && window.Blob) {
+    alert('Supported');
   }
   console.log(`clicked ${file}`);
+}
+
+/**
+ * Handles the file change
+ * @param {Event} evt the event
+ */
+function handleFileSelect(evt) {
+  const file = evt.target.files[0];
+
+  // Only process image files.
+  if (!file.type.match('image.*')) {
+    alert('Unsupported Image File');
+    document.getElementById('form').reset();
+    return;
+  }
+
+  const reader = new FileReader();
+  reader.onload = (event) => {
+    const postData = {};
+    postData.data = event.target.result;
+    postData.type = file.type;
+
+    const xhr = new XMLHttpRequest();
+    const url = '/gcode';
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        console.log(xhr.responseText);
+      } else if (xhr.readyState === 4 && xhr.status === 400) {
+        alert(`Error: ${xhr.responseText}`);
+        document.getElementById('image_form').reset();
+      }
+    };
+
+    xhr.onerror = () => {
+      console.log('error');
+      alert(`Error: ${xhr.responseText}`);
+    };
+
+    xhr.onabort = () => {
+      alert('aborted');
+    };
+
+    xhr.send(JSON.stringify(postData));
+  };
+
+  // Read in the image file as binary string.
+  reader.readAsBinaryString(file);
 }
